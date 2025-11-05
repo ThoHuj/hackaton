eeea3d2ef42d2c6df560515261b8d4772a73a8d3
ThoHuj <nyheterna-vid.0v@icloud.com>
2025-11-05 14:00:23 +0100

    Making it possible to generate a database.

--- wordfetcher.py
+++ wordfetcher.py
@@ -5,6 +5,14 @@ import os
 #word_url = requests.get("https://random-word-api.herokuapp.com/word?length=5")
 #dictionary_url = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/")
 
+class WordFetcher():
+    def __init__(self, word_length: int, word_amount: int):
+        self.word_length = word_length
+        self.word_amount = word_amount
+
+    def return_word(self) -> list[str]:
+        return requests.get(url=f"https://random-word-api.herokuapp.com/word?length={self.word_length}&number={self.word_amount}").json()
+
 
 # This class fetches word of the day from a Wordly api
 class WordleGame:
